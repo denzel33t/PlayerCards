d2509e189cfbbf7fa4d85b92ddfd9a3612822910
denzel33t <denzel.eggerue@gmail.com>
2022-04-21 22:48:49 +0100

    created playercard repo

---
+++ src/main/java/com/example/demo/repo/PlayerCardRepo.java
@@ -0,0 +1,17 @@
+package com.example.demo.repo;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.domain.PlayerCard;
+
+@Repository
+public interface PlayerCardRepo extends JpaRepository<PlayerCard, Integer> {
+	// Automatically generates CRUD functions 
+	List<PlayerCard> findByPlayerPositionIgnoreCase(String playerPosition);
+	
+	List<PlayerCard> findByFootballClubIgnoreCase(String footballClub);
+
+}
